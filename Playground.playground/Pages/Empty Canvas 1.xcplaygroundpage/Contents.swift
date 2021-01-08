12049b683b89507df294ce4e383cd56207416543
Sierra <sgilfillan@lcs.on.ca>
2021-01-08 10:16:36 -0500

    First step: Panda head

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -42,7 +42,7 @@ canvas.drawAxes(withScale: true, by: 50)
 canvas.drawRectangle(at: Point(x: 50, y: 75), width: 100, height: 200)
 
 canvas.drawShapesWithBorders = true
-
+canvas.
 canvas.fillColor = Color.white
 canvas.drawEllipse(at: Point(x: 150, y: 400), width: 100, height: 100)
 
