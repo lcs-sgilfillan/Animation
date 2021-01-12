d10ecec37333b5fd8f90c7fe48f5f7bebed106d3
Sierra <sgilfillan@lcs.on.ca>
2021-01-11 21:01:07 -0500

    Body + arms and legs

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -40,22 +40,58 @@ PlaygroundPage.current.liveView = canvas
 canvas.drawAxes(withScale: true, by: 50)
 
 
+//body
+canvas.drawEllipse(at: Point(x: 150, y: 300), width: 170, height: 200)
 
+
+//Head
 canvas.drawEllipse(at: Point(x: 185, y: 430), width: 30, height: 30)
 
-
+//
 canvas.drawShapesWithBorders = true
 canvas.drawEllipse(at: Point(x: 115, y: 430), width: 30, height: 30)
+
 
 canvas.fillColor = Color.white
 canvas.drawEllipse(at: Point(x: 150, y: 400), width: 100, height: 100)
 
+//arms and legs
+canvas.drawEllipse(at: Point(x: 70, y: 340), width: 40, height: 40)
+canvas.drawEllipse(at: Point(x: 230, y: 340), width: 40, height: 40)
+canvas.drawEllipse(at: Point(x: 80, y: 230), width: 50, height: 70)
+canvas.drawEllipse(at: Point(x: 220, y: 230), width: 50, height: 70)
+
+//outter eyes
 canvas.drawEllipse(at: Point(x: 130, y: 410), width: 20, height: 30)
 canvas.drawEllipse(at: Point(x: 170, y: 410), width: 20, height: 30)
 
+//ears
 canvas.fillColor = Color.black
 canvas.drawEllipse(at: Point(x: 130, y: 405), width: 20, height: 20)
 canvas.drawEllipse(at: Point(x: 170, y: 405), width: 20, height: 20)
+
+//prints
+//left arm
+canvas.drawEllipse(at: Point(x: 57, y: 345), width: 8, height: 10)
+canvas.drawEllipse(at: Point(x: 70, y: 350), width: 8, height: 10)
+canvas.drawEllipse(at: Point(x: 83, y: 345), width: 8, height: 10)
+canvas.drawEllipse(at: Point(x: 70, y: 335), width: 16, height: 13)
+//right arm
+canvas.drawEllipse(at: Point(x: 220, y: 345), width: 8, height: 10)
+canvas.drawEllipse(at: Point(x: 231, y: 350), width: 8, height: 10)
+canvas.drawEllipse(at: Point(x: 244, y: 345), width: 8, height: 10)
+canvas.drawEllipse(at: Point(x: 231, y: 335), width: 16, height: 13)
+//left leg
+canvas.drawEllipse(at: Point(x: 65, y: 245), width: 10, height: 12)
+canvas.drawEllipse(at: Point(x: 82, y: 252), width: 10, height: 12)
+canvas.drawEllipse(at: Point(x: 95, y: 245), width: 10, height: 12)
+canvas.drawEllipse(at: Point(x: 80, y: 230), width: 20, height: 20)
+canvas.drawEllipse(at: Point(x: 75, y: 225), width: 15, height: 15)
+canvas.drawEllipse(at: Point(x: 85, y: 225), width: 15, height: 15)
+//right leg
+canvas.drawEllipse(at: Point(x: 220, y: 245), width: 10, height: 12)
+canvas.drawEllipse(at: Point(x: 237, y: 252), width: 10, height: 12)
+canvas.drawEllipse(at: Point(x: 250, y: 245), width: 10, height: 12)
 
 /*:
  ## Show the Live View
