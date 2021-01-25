c4c1fd62054e49c4b8a3b68a611c420a6e317639
Sierra <sgilfillan@lcs.on.ca>
2021-01-25 10:04:50 -0500

    Pixies poster text + started the white circles

--- Playground.playground/Pages/Emtpy Canvas 4.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Emtpy Canvas 4.xcplaygroundpage/Contents.swift
@@ -36,8 +36,6 @@ PlaygroundPage.current.liveView = canvas
 
  */
 
-// Grid/scale
-canvas.drawAxes(withScale: true, by: 50)
 
 
 //Top green rectangle
@@ -56,29 +54,68 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 400)
 
 
 for x in stride(from: 0,
-                to: 400,
+                through: 400,
                 by: 40) {
     
     for y in stride(from: 0,
-                    to: 400,
+                    through: 400,
                     by: 40) {
         
         canvas.fillColor = Color(hue: 106,saturation: 64, brightness: 73,
             alpha: 100)
         
-        canvas.drawEllipse(at: Point(x: x + 10, y: y + 20),  width: 40, height: 40)
-
-    
+        canvas.drawEllipse(at: Point(x: x + 10, y: y + 0),  width: 40, height: 40)
     }
 
+//Draw white cicrle
+        
+    for x in stride(from: 40,
+                    through: 350,
+                    by: 40) {
+        
+        for y in stride(from: 40,
+                        through: 350,
+                        by: 40) {
+            
+    
+    canvas.fillColor = Color.white
+    canvas.drawEllipse (at: Point(x: x + 10, y: y + 40),  width: 40, height: 40)
+}
 
+//white rectangle
 
+canvas.fillColor = Color.white
 
+canvas.drawRectangle(at: Point(x: 400, y: 0), width: 50, height: 600)
+    
+   
+    
+        
+}
 
+    
+    // Grid/scale
+    canvas.drawAxes(withScale: true, by: 50)
 
+//Text
+    canvas.drawText(message: "pixies", at: Point(x: 50, y: 420), size: 70, kerning: 0.0)
 
-
-
+    
+    canvas.drawText(message: "at the rat", at: Point(x: 285, y: 550), size: 10, kerning: 0.0)
+    
+    canvas.drawText(message: "528 commonwealth", at: Point(x: 285, y: 530), size: 10, kerning: 0.0)
+    
+    canvas.drawText(message: "Boston, mass.", at: Point(x: 285, y: 520), size: 10, kerning: 0.0)
+    
+    //White text
+    
+    canvas.fillColor = Color.white
+    
+    canvas.drawText(message: "with", at: Point(x: 285, y: 490), size: 12, kerning: 0.0)
+    
+    canvas.drawText(message: "throughing muses", at: Point(x: 285, y: 470), size: 12, kerning: 0.0)
+    
+    canvas.drawText(message: "big dipper", at: Point(x: 285, y: 450), size: 12, kerning: 0.0)
 
 
 
@@ -99,4 +136,6 @@ for x in stride(from: 0,
  
  ![source_control](source-control.png "Source Control")
  */
-    }
+
+
+}
